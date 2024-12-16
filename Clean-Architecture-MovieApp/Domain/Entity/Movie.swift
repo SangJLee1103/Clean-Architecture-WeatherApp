77bab1b68100454725c9184e320cf91285ab3c9c
이상준 <>
2024-12-16 21:53:19 +0900

    feat: Domain Layer 구현

---
+++ Clean-Architecture-MovieApp/Domain/Entity/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  Clean-Architecture-MovieApp
+//
+//  Created by 이상준 on 12/14/24.
+//
+
+import Foundation
+
+struct Movie {
+    let title: String
+    let overview: String
+    let posterPath: String
+    let voteAverage: Double
+}
